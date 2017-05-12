a858d14c19ad3b48f2098292b49ce1a9c90e04b6
Igor <igor.filipiuk@gmail.com>
2017-05-12 02:33:27 +0200

    added buttons in RiddlePage

--- app/src/main/java/edu/pjwstk/ifpk/nocmuzeowapp/RiddlePage.java
+++ app/src/main/java/edu/pjwstk/ifpk/nocmuzeowapp/RiddlePage.java
@@ -16,7 +16,7 @@ public class RiddlePage extends Page {
     TextView descriptionTV;
     ImageView imageIV;
     Button buttonBT;
-
+    Button mapaBttn, skanerBttn;
 
     RiddlePage(Activity activity,HeroAdapter heroes){
         ctx = activity;
@@ -24,6 +24,21 @@ public class RiddlePage extends Page {
         nameTV = (TextView)ctx.findViewById(R.id.riddle_name);
         descriptionTV = (TextView)ctx.findViewById(R.id.riddle_text);
         imageIV = (ImageView)ctx.findViewById(R.id.riddle_image);
+
+        mapaBttn = (Button)ctx.findViewById(R.id.mapaBttn);
+        mapaBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)ctx).changeSelectedPage(MainActivity.FLIP_MAP,true);
+            }
+        });
+        skanerBttn = (Button)ctx.findViewById(R.id.skanerBttn);
+        skanerBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)ctx).changeSelectedPage(MainActivity.FLIP_SCAN,true);
+            }
+        });
 
     }
     @Override
