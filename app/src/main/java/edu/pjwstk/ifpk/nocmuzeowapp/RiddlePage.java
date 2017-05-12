e6e73d73e100a410ec330fef17bbe659e34e3e11
Igor <igor.filipiuk@gmail.com>
2017-05-12 02:17:28 +0200

    added new icons, new buttons in flip_riddle

--- app/src/main/java/edu/pjwstk/ifpk/nocmuzeowapp/RiddlePage.java
+++ app/src/main/java/edu/pjwstk/ifpk/nocmuzeowapp/RiddlePage.java
@@ -28,7 +28,8 @@ public class RiddlePage extends Page {
     TextView nameTV;
     TextView descriptionTV;
     ImageView imageIV;
-    Button buttonBT;
+   // Button buttonBT;
+    Button mapaBttn, skanerBttn;
 
 
     RiddlePage(Activity activity,HeroAdapter heroes){
@@ -38,11 +39,25 @@ public class RiddlePage extends Page {
         descriptionTV = (TextView)ctx.findViewById(R.id.riddle_text);
         imageIV = (ImageView)ctx.findViewById(R.id.riddle_image);
 
-        buttonBT = (Button)ctx.findViewById(R.id.riddle_button);
-        buttonBT.setOnClickListener(new View.OnClickListener() {
+       // buttonBT = (Button)ctx.findViewById(R.id.riddle_button);
+       /* buttonBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 nextRiddle();
+            }
+        }); */
+       mapaBttn = (Button)ctx.findViewById(R.id.mapaBttn);
+        mapaBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)ctx).changeSelectedPage(MainActivity.FLIP_MAP,true);
+            }
+        });
+        skanerBttn = (Button)ctx.findViewById(R.id.skanerBttn);
+        skanerBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)ctx).changeSelectedPage(MainActivity.FLIP_SCAN,true);
             }
         });
     }
